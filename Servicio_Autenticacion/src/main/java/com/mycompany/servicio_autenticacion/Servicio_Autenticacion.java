d23a8a4da43eb19b41689e675fddea7d54879b47
Ramon Valencia <199168436+RamonVH18@users.noreply.github.com>
2026-04-26 19:04:32 -0700

    Inicio Proyecto

---
+++ Servicio_Autenticacion/src/main/java/com/mycompany/servicio_autenticacion/Servicio_Autenticacion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.servicio_autenticacion;
+
+/**
+ *
+ * @author Ramon Valencia 
+ */
+public class Servicio_Autenticacion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Servicio_Permisos/src/main/java/com/mycompany/servicio_permisos/Servicio_Permisos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.servicio_permisos;
+
+/**
+ *
+ * @author Ramon Valencia 
+ */
+public class Servicio_Permisos {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
